a16cc5b598c4fd888518bbcf712bc4b848f0ccc4
Nathalie Fajardo <nathaliefc89@gmail.com>
2025-12-08 19:39:54 +0000

    adding viteconfig base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/WEB_LIQUIDADORA_LYAH/",
+  base: "/liquidadora-lyah/",
   plugins: [react()],
 });
